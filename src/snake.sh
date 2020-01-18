ba16e6caf7efb85bc88f40d99404f15c8ad23d35
197708156EQUJ5 <1047196+197708156EQUJ5@users.noreply.github.com>
2020-01-18 17:06:09 -0500

    [BHSNK-1] Snake moves around

--- src/snake.sh
+++ src/snake.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
+source lib/utils
+source lib/board
+source lib/cursor_move
+
 function main() {
     echo "Welcome to Snake"
+    init_screen
+    trap refresh_board ALRM
+    refresh_board
+    while :
+    do
+        read -s -n 1 key
+        
+        if [[ $key = q ]]; then
+            goodbye
+            break
+        elif [[ $key = a ]]; then
+            change_direction 4
+        elif [[ $key = s ]]; then
+            change_direction 3
+        elif [[ $key = d ]]; then
+            change_direction 2
+        elif [[ $key = w ]]; then
+            change_direction 1
+        fi
+    done
 }
 
 main
